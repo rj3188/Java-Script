cb4e72d48782c880053b6acae70d7fa285b4843e
rj3188 <140106945+rj3188@users.noreply.github.com>
2023-07-24 08:59:18 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,37 @@
+// Example 1
+
+function Name() {
+    console.log('Hello World')
+};
+
+Name();
+
+
+// Example 2
+
+function greet(name, lastName) {
+    console.log('Hello ' +name+' '+lastName+'');
+}
+
+greet('Jai ', 'Rathod');
+
+
+// Example 3 
+
+function name(surName) {
+    console.log('Jigar ' +surName+'');
+}
+
+name('Rathod');
+
+
+// Example 4
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+
+console.log(number);
+
